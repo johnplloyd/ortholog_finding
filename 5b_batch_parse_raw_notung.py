ca35c76cb293e6214e1cfbd36bdb913f3248896b
John Lloyd <john.paul.lloyd@gmail.com>
2020-05-10 17:21:33 -0400

    Update 5b_batch_parse_raw_notung.py

--- 5b_batch_parse_raw_notung.py
+++ 5b_batch_parse_raw_notung.py
@@ -1,14 +1,15 @@
 print'''
 Inputs:
 	1 - Directory with .reconciled files
+	2 - TreeUtility.py location
 '''
 
 import os, sys
 
 dir = os.path.abspath(sys.argv[1])
-
+TreeUtility_loc = sys.argv[2]
 
 for file in os.listdir(dir):
     if file.endswith('.reconciled'):
-        os.system('python /mnt/home/lloydjo1/scripts/Shiu_Scripts/TreeUtility.py -f parse_notung -t %s/%s' % (dir,file))
+        os.system('python %s -f parse_notung -t %s/%s' % (TreeUtility_loc,dir,file))
 
