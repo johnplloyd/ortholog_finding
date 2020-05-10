ce9f31bcb6eb3af73750fc838a5ee4ff18be990e
John Lloyd <john.paul.lloyd@gmail.com>
2020-05-10 17:18:34 -0400

    Update 5_write_notung_job.py

--- 5_write_notung_job.py
+++ 5_write_notung_job.py
@@ -3,16 +3,18 @@ Inputs:
 	1 - Directory with .midpoint files
 	2 - Output directory
 	3 - Species tree
+	4 - Notung jar location
 '''
 import os,sys
 
 dir_in = os.path.abspath(sys.argv[1])
 dir_out = os.path.abspath(sys.argv[2])
 species_tree = sys.argv[3]
+notung_jar_loc = sys.argv[4]
 
 for file in os.listdir(dir_in):
     if file.endswith('.midpoint'):
-        os.system('java -jar /mnt/home/lloydjo1/bin/Notung-2.6/Notung-2.6.jar '\
+        os.system('java -jar %s '\
 		'-g %s/%s -s %s --reconcile -speciestag prefix -edgeweights length '\
-		'--outputdir %s' % (dir_in,file,species_tree,dir_out))
+		'--outputdir %s' % (notung_jar_loc,dir_in,file,species_tree,dir_out))
 
